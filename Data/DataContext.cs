ce2ce0b7fd5ddf4144077b00fb4fa4aef6ba860f
ABID HAMID <amnesiaagain@googlemail.com>
2020-02-13 22:41:30 +0000

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using AmigosAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmigosAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options) {}
+        public DbSet <Product> Products { get; set; }
+    }
+}
